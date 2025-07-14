479f5aeb67aa06a8e9c072f0a1e46d7ecf2dcc73
xiadnoring <timurtuty132@gmail.com>
2025-07-14 12:05:21 +0500

    UPD | comments #4

--- examples/self-signed-ssl/generate.sh
+++ examples/self-signed-ssl/generate.sh
@@ -1,16 +1,9 @@
-## Generate keys
-#
-#openssl req -x509 -newkey rsa:2048 -keyout cert.key -out cert.crt -days 999 -subj "/C=RU/ST=Bashkort Republic/L=Ufa/O=Manapi Inc/OU=Manapi Http/CN=localhost"
-#openssl rsa -in cert.key -out cert.key.new
-#
-## Generate Root Certificate
-#mv cert.key.new cert.key
-## You can import this file into your browser or system for testing purposes only
-#cat cert.key cert.crt > rootca.crt
-
 # Generate keys
-openssl req -x509 -nodes -days 999 -newkey rsa:2048 -keyout cert.key -out cert.crt
-rm rootca.crt
+
+openssl req -x509 -newkey rsa:2048 -keyout cert.key -out cert.crt -days 999 -subj "/C=RU/ST=Bashkort Republic/L=Ufa/O=Manapi Inc/OU=Manapi Http/CN=localhost" -addext "subjectAltName = DNS:localhost, IP:127.0.0.1, email:me@timurtuty.ru"
+openssl rsa -in cert.key -out cert.key.new
+
 # Generate Root Certificate
+mv cert.key.new cert.key
 # You can import this file into your browser or system for testing purposes only
 cat cert.key cert.crt > rootca.crt
