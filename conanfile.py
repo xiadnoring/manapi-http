5dda1df48480173e27608c1ba5e74aab789adeb0
xiadnoring <timurtuty132@gmail.com>
2025-10-13 11:07:25 +0500

    UPD | new version

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ import os
 class ManapiHttpConan(ConanFile):
     name = "manapihttp"
     description = "Fast http server/client"
-    version = "1.0.1"
+    version = "1.0.2"
 
     settings = "os", "compiler", "build_type", "arch"
 
