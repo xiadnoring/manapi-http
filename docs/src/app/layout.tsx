54374f7a0e917e1279d6f82bada651264c69c772
xiadnoring <timurtuty132@gmail.com>
2025-11-28 12:11:27 +0300

    UPD | docs #6

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
+import SearchDialog from '@/components/search';
+
 const inter = Inter({
   subsets: ['latin'],
 });
@@ -12,9 +14,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider search={{
-          options: {
-            type: "static"
-          }
+          enabled: false,
         }}>{children}</RootProvider>
       </body>
     </html>
