592906bd75d8f2be6735d477c96c047af8a11d94
xiadnoring <timurtuty132@gmail.com>
2025-11-28 12:18:50 +0300

    UPD | docs #8

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -1,13 +1,6 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { BookIcon } from 'lucide-react';
 
-/**
- * Shared layout configurations
- *
- * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/docs/layout.tsx
- */
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
