de542e07cbc820996077afbab00adefd28ba4aec
xiadnoring <timurtuty132@gmail.com>
2026-07-14 13:28:58 +0300

    UPD | json refactoring #3

--- src/ManapiUnicode.cpp
+++ src/ManapiUnicode.cpp
@@ -15,7 +15,7 @@ static const unsigned char hextable[] = {
     0, 10, 11, 12, 13, 14, 15                             /* 0x60 - 0x66 */
 };
 
-static const unsigned char hextable2[] = {
+static constexpr unsigned char hextable2[] = {
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 };
 
