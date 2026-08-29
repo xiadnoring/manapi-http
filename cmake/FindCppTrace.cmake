ef58d745a55699b03f3d48692a5c565b5585b254
xiadnoring <timurtuty132@gmail.com>
2026-08-30 00:13:51 +0500

    UPD | paths to find cpptrace

--- cmake/FindCppTrace.cmake
+++ cmake/FindCppTrace.cmake
@@ -1,13 +1,29 @@
 find_path(CPPTRACE_INCLUDE_DIRS
         NAMES cpptrace/cpptrace.hpp
+        HINTS
+            $ENV{HOME}/wherever
+            $ENV{HOME}/wherever/include
 )
+
 find_library(CPPTRACE_LIBRARIES
         NAMES cpptrace
+        HINTS
+            $ENV{HOME}/wherever
+            $ENV{HOME}/wherever/lib
+            $ENV{HOME}/wherever/lib64
+)
+
+find_library(CPPTRACE_DWARF_LIBRARIES
+        NAMES dwarf
+        HINTS
+            $ENV{HOME}/wherever
+            $ENV{HOME}/wherever/lib
+            $ENV{HOME}/wherever/lib64
 )
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(CppTrace REQUIRED_VARS
-        CPPTRACE_LIBRARIES CPPTRACE_INCLUDE_DIRS)
+        CPPTRACE_LIBRARIES CPPTRACE_INCLUDE_DIRS CPPTRACE_DWARF_LIBRARIES)
 
-mark_as_advanced(CPPTRACE_INCLUDE_DIRS CPPTRACE_LIBRARIES)
+mark_as_advanced(CPPTRACE_INCLUDE_DIRS CPPTRACE_LIBRARIES CPPTRACE_DWARF_LIBRARIES)
