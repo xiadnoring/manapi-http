f7f4d0a86fddad5115d4c0f13636ee248aa5fa3f
xiadnoring <timurtuty132@gmail.com>
2026-08-29 19:08:59 +0500

    UPD | tests

--- tests/test_http_and_fetch_keep_alive.cpp
+++ tests/test_http_and_fetch_keep_alive.cpp
@@ -24,7 +24,7 @@ UTEST (http_and_fetch, keep_alive_1) {
             {"speed_check_bytes", 100000000}
         }}
     }, [&] () -> manapi::future<> {
-        for (int i =0 ; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             if (i) {
                 co_await manapi::async::delay{1500};
             }
